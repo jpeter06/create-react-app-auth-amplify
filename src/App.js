0bed05f831476c30a37d9f0368728a6ce0ee3e1f
Juan Pedro de Andres Díaz <juan.andres@ree.es>
2022-06-10 09:13:11 +0200

    en Español

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Editar <code>src/App.js</code> y salvar para recargar.
           </p>
           <a
             className="App-link"
